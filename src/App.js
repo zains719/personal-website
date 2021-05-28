48dccf44cf73077931d39a25d0dd94037776051a
Zain Saleem <zain@zains-mbp.home>
2021-05-28 12:21:00 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
             <a href="https://github.com/zains719"><Github /></a>
             <a href="https://www.linkedin.com/in/zain-saleem-137603211/"><LinkedIn /></a>
             <a href="https://www.instagram.com/_zain719/"><Instagram /></a>
-            <a href="https://s3.amazonaws.com/attachments.angel.co/5899015-77290018c104b5f57cf427c7b44cbfac.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJS6W3HGZGRJIRBTA%2F20210527%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210527T111720Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=5e48e582afd3c694b2122e347690090818413421179f0b9f87352c50cfd5109a">
+            <a href="/Zain-Saleem-CV.pdf" download>
               <Resume />
             </a>
           </div>
